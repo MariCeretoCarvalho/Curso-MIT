1b8fcdefa7e4b481f168b1c9b582c6c9585b2dc9
Marianna <marianna@cereto.com>
2024-11-26 16:48:36 -0800

    exercicios

---
+++ node_ae57027e9ccde.tmp.js
@@ -0,0 +1,19 @@
+//const currentYear = 2024;
+//console.log('current year result: ', currentYear);
+//module.exports = { currentYear };
+
+function fibonacci(n) {
+    // Base case: If n is 0 or 1, return n itself.
+    if (n === 0 || n === 1)
+        return n;
+ 
+    // Recursive case: Calculate fibonacci(n-1) and fibonacci(n-2) and add them.
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+ 
+const n = 1567;
+const result = fibonacci(1567);
+console.log(`The ${n}-th fibonacci number is ${result}`);
+// The 6-th fibonacci number is 8
+
--- variables-02.js
+++ variables-02.js
@@ -1,3 +1,39 @@
-const currentYear = 2024;
-console.log('current year result: ', currentYear);
-module.exports = { currentYear };
+//const currentYear = 2024;
+//console.log('current year result: ', currentYear);
+//module.exports = { currentYear };
+
+function fibonacci_slow(n) {
+    // Base case: If n is 0 or 1, return n itself.
+    if (n === 0 || n === 1)
+        return n;
+ 
+    // Recursive case: Calculate fibonacci(n-1) and fibonacci(n-2) and add them.
+    else {
+        let result = fibonacci_slow(n - 1) + fibonacci_slow(n - 2);
+        return result;
+    }
+}
+
+
+let memo = {};
+function fibonacci(n) {
+    if (n in memo) {
+        return memo[n];
+    }
+    // Base case: If n is 0 or 1, return n itself.
+    if (n === 0 || n === 1)
+        return n;
+ 
+    // Recursive case: Calculate fibonacci(n-1) and fibonacci(n-2) and add them.
+    else {
+        let result = fibonacci(n - 1) + fibonacci(n - 2);
+        memo[n] = result;
+        return result;
+    }
+}
+ 
+const n = 6;
+const result = fibonacci(6);
+console.log(`The ${n}-th fibonacci number is ${result}`);
+// The 6-th fibonacci number is 8
+
